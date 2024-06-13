9f9b3ac9422e8d9e4ca58e697ab0becf499d4d62
Mohammed Zeeshan <work.zeeshan@gmail.com>
2024-06-13 14:32:46 +0530

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
   output: "server",
+  adapter: netlify()
 });
